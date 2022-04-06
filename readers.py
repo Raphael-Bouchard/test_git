eac59f8d1ee37b0bbd0313e3ecf2088f40f69627
Raphael BOUCHARD <rbouchard@hotmail.fr>
2022-04-06 22:29:35 +0200

    suppresion de certains commentaires

--- readers.py
+++ readers.py
@@ -22,10 +22,6 @@ writer = FFMpegWriter(fps=15, metadata=metadata)
 ########################################################
 
 
-##############################""
-# ajout de lignes de commentaires pour vérifier si je comprends guthub
-#deuxieme test dan sla branches 
-############################
 
 
 
